bc78eea2bb1b35258d365448c782eabcca20b3c1
Daniel Ivanov <didakis2001@gmail.com>
2024-07-31 18:29:13 +0300

    Show books from database in the frontend

--- frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -4,7 +4,7 @@ import BookModel from "../../../models/BookModel";
 
 export const Carousel = () => {
 
-    const [book, setBooks] = useState<BookModel[]>([]);
+    const [books, setBooks] = useState<BookModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
 
@@ -34,7 +34,7 @@ export const Carousel = () => {
                     description: responseData[key].description,
                     copies: responseData[key].copies,
                     copiesAvailable: responseData[key].copiesAvailable,
-                    category:responseData[key].category,
+                    category: responseData[key].category,
                     img: responseData[key].img
                 });
             }
@@ -48,16 +48,16 @@ export const Carousel = () => {
         })
     }, []);
 
-    if(isLoading){
-        return(
+    if (isLoading) {
+        return (
             <div className="container m-5">
                 <p>Loading...</p>
             </div>
         )
     }
 
-    if(httpError){
-        return(
+    if (httpError) {
+        return (
             <div className="container m-5">
                 <p>{httpError}</p>
             </div>
@@ -76,23 +76,23 @@ export const Carousel = () => {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <div className="row d-felx justify-content-center align-items-center">
-                            <ReturnBook />
-                            <ReturnBook />
-                            <ReturnBook />
+                            {books.slice(0, 3).map(book => (
+                                <ReturnBook book={book} key={book.id} />
+                            ))}
                         </div>
                     </div>
                     <div className="carousel-item">
                         <div className="row d-felx justify-content-center align-items-center">
-                            <ReturnBook />
-                            <ReturnBook />
-                            <ReturnBook />
+                            {books.slice(3, 6).map(book => (
+                                <ReturnBook book={book} key={book.id} />
+                            ))}
                         </div>
                     </div>
                     <div className="carousel-item">
                         <div className="row d-felx justify-content-center align-items-center">
-                            <ReturnBook />
-                            <ReturnBook />
-                            <ReturnBook />
+                            {books.slice(6, 9).map(book => (
+                                <ReturnBook book={book} key={book.id} />
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -110,7 +110,7 @@ export const Carousel = () => {
             {/* Mobile*/}
             <div className="d-lg-none mt-3">
                 <div className="row d-flex justify-content-center align-items-center">
-                    <ReturnBook />
+                    <ReturnBook book={books[7]} key={books[7].id}/>
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
