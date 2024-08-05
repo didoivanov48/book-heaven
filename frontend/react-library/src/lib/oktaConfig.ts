1dc66fc830bd272f6fd7bd72b7ea27ae8b6ab751
Daniel Ivanov <didakis2001@gmail.com>
2024-08-05 15:35:56 +0300

    Create oktaConfig.ts

---
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: "0oairlu650IZlgBob5d7",
+    issuer: "https://dev-71735817.okta.com/oauth2/default",
+    redirectUri: "http://localhost:3000/login/callback",
+    scopes: ["opedid", "profile", "email"],
+    pkce: true,
+    disableHttpsCheck: true,
+}
