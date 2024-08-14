b6b6d4da3125c9a335a553721069b4592811940e
Daniel Ivanov <didakis2001@gmail.com>
2024-08-14 13:46:28 +0300

    Update oktaConfig.ts

--- frontend/react-library/src/lib/oktaConfig.ts
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -1,8 +1,8 @@
 export const oktaConfig = {
-    clientId: "0oairlu650IZlgBob5d7",
-    issuer: "https://dev-71735817.okta.com/oauth2/default",
-    redirectUri: "http://localhost:3000/login/callback",
-    scopes: ["openid", "profile", "email"],
+    clientId: '0oaiwcre4gHh6M8Fz5d7',
+    issuer: 'https://dev-10072743.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
 }
