ba82664f094c9136810f13543aae09f16c82123d
Daniel Ivanov <didakis2001@gmail.com>
2024-08-15 16:30:07 +0300

    Create Checkout Repository

---
+++ backend/spring-boot-library/src/main/java/com/bookheaven/springbootlibrary/dao/CheckoutRepository.java
@@ -0,0 +1,9 @@
+package com.bookheaven.springbootlibrary.dao;
+
+import com.bookheaven.springbootlibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
