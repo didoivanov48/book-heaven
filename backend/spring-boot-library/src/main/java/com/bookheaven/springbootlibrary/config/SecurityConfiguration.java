0e1479553853f22375137a2d9562648b8c38ef4b
Daniel Ivanov <didakis2001@gmail.com>
2024-08-15 18:24:37 +0300

    Update pom.xml and add Security Configuration

---
+++ backend/spring-boot-library/src/main/java/com/bookheaven/springbootlibrary/config/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.bookheaven.springbootlibrary.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // Disable Cross Site Request Forgery
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // Protect endpoints at /api/<type>/secure
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/api/books/secure/**").authenticated()
+                                .requestMatchers("/api/books/**", "/api/reviews/**").permitAll())
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        // Add CORS filters
+        http.cors(Customizer.withDefaults());
+
+        //  Add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+        // Force a non-empty response body to make unauthorized response body more friendly.
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }
+
+}
