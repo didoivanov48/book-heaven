009b42a133e01c909dd2db683ff6f2fb859e09ac
Daniel Ivanov <didakis2001@gmail.com>
2024-08-15 19:50:40 +0300

    Update Book Checkout Page

--- frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -62,7 +62,7 @@ export const BookCheckoutPage = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    }, []);
+    }, [isCheckedOut]);
 
     useEffect(() => {
         const fetchBookReviews = async () => {
@@ -133,11 +133,11 @@ export const BookCheckoutPage = () => {
             setIsLoadingCurrentLoansCount(false);
             setHttpError(error.message);
         })
-    }, [authState]);
+    }, [authState, isCheckedOut]);
 
     useEffect(() => {
         const fetchUserCheckedOutBook = async () => {
-            if(authState && authState.isAuthenticated){
+            if (authState && authState.isAuthenticated) {
                 const url = `http://localhost:8080/api/books/secure/ischeckedout/byuser?bookId=${bookId}`;
                 const requestOptions = {
                     method: "GET",
@@ -148,7 +148,7 @@ export const BookCheckoutPage = () => {
                 };
                 const bookCheckedOut = await fetch(url, requestOptions);
 
-                if(!bookCheckedOut.ok){
+                if (!bookCheckedOut.ok) {
                     throw new Error("Something went wrong!")
                 }
 
@@ -177,6 +177,22 @@ export const BookCheckoutPage = () => {
         )
     }
 
+    async function checkoutBook() {
+        const url = `http://localhost:8080/api/books/secure/checkout?bookId=${book?.id}`
+        const requestOptions = {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                "Content-Type": "application/json"
+            }
+        };
+        const checkoutResponse = await fetch(url, requestOptions);
+        if (!checkoutResponse.ok) {
+            throw new Error("Something went wrong!");
+        }
+        setIsCheckedOut(true);
+    }
+
     return (
         <div>
             <div className="container d-none d-lg-block">
@@ -197,8 +213,9 @@ export const BookCheckoutPage = () => {
                             <StarsReview rating={totalStars} size={32} />
                         </div>
                     </div>
-                    <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount} 
-                    isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}/>
+                    <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount}
+                        isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
+                        checkoutBook={checkoutBook} />
                 </div>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -220,8 +237,9 @@ export const BookCheckoutPage = () => {
                         <StarsReview rating={totalStars} size={32} />
                     </div>
                 </div>
-                <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount} 
-                isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} />
+                <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}
+                    isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
+                    checkoutBook={checkoutBook} />
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
             </div>
